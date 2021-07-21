a76fdf7f3f5df73a05e83bbb1455cbdae6849412
Basweshwar Amlapure <baswraj@gmail.com>
2021-07-21 08:27:16 +0530

    core java file added by Bas

---
+++ src/corejavaprogramms/DemoConstructer.java
@@ -0,0 +1,16 @@
+package corejavaprogramms;
+
+public class DemoConstructer {
+
+	public DemoConstructer() {
+		// TODO Auto-generated constructor stub
+		System.out.println("I am in constructer of class");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DemoConstructer ob=new DemoConstructer();
+
+	}
+
+}
